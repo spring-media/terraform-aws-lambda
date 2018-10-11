9c7d23182e949e79f9b05839fb9070ed2bd45c56
Moritz Zimmer <moritz.zmmr@gmail.com>
2018-10-11 11:03:55 +0200

    Reorderd and sorted variables

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
-variable "name" {
-  description = "A unique name for this Lambda module."
-}
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
 
 variable "function_name" {
   description = "A unique name for your Lambda Function."
 }
 
-variable "description" {
-  description = "Description of what your Lambda Function does."
-  default     = ""
+variable "name" {
+  description = "A unique name for this Lambda module."
 }
 
 variable "s3_bucket" {
@@ -19,19 +19,13 @@ variable "s3_key" {
   description = "The S3 key of an object containing the function's deployment package."
 }
 
-variable "timeout" {
-  description = "The amount of time your Lambda Function has to run in seconds. Defaults to 3."
-  default     = 3
-}
-
-variable "runtime" {
-  description = "The runtime environment for the Lambda function you are uploading. Defaults to go1.x"
-  default     = "go1.x"
-}
-
-variable "memory_size" {
-  description = "Amount of memory in MB your Lambda Function can use at runtime. Defaults to 128."
-  default     = 128
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+variable "description" {
+  description = "Description of what your Lambda Function does."
+  default     = ""
 }
 
 variable "handler" {
@@ -39,8 +33,8 @@ variable "handler" {
   default     = ""
 }
 
-variable "schedule_expression" {
-  description = "An optional scheduling expression for triggering the Lambda Function using CloudWatch events. For example, cron(0 20 * * ? *) or rate(5 minutes)."
+variable "kms_key_arn" {
+  description = "The Amazon Resource Name (ARN) of the KMS key to decrypt AWS Systems Manager parameters."
   default     = ""
 }
 
@@ -54,24 +48,19 @@ variable "logfilter_destination_arn" {
   default     = ""
 }
 
-variable "stream_batch_size" {
-  description = "The largest number of records that Lambda will retrieve from your event source at the time of invocation. Defaults to 100."
-  default     = 100
+variable "memory_size" {
+  description = "Amount of memory in MB your Lambda Function can use at runtime. Defaults to 128."
+  default     = 128
 }
 
-variable "stream_event_source_arn" {
-  description = "An optional event source ARN - can either be a Kinesis or DynamoDB stream."
+variable "runtime" {
+  description = "The runtime environment for the Lambda function you are uploading. Defaults to go1.x"
+  default     = "go1.x"
+}
+
+variable "schedule_expression" {
+  description = "An optional scheduling expression for triggering the Lambda Function using CloudWatch events. For example, cron(0 20 * * ? *) or rate(5 minutes)."
   default     = ""
-}
-
-variable "stream_enabled" {
-  description = "This enables creation of a stream event source mapping for the Lambda function. Defaults to false."
-  default     = false
-}
-
-variable "stream_starting_position" {
-  description = "The position in the stream where AWS Lambda should start reading. Must be one of either TRIM_HORIZON or LATEST. Defaults to TRIM_HORIZON."
-  default     = "TRIM_HORIZON"
 }
 
 variable "ssm_parameter_names" {
@@ -79,13 +68,33 @@ variable "ssm_parameter_names" {
   default     = []
 }
 
-variable "kms_key_arn" {
-  description = "The Amazon Resource Name (ARN) of the KMS key to decrypt AWS Systems Manager parameters."
+variable "stream_batch_size" {
+  description = "The largest number of records that Lambda will retrieve from your event source at the time of invocation. Defaults to 100."
+  default     = 100
+}
+
+variable "stream_enabled" {
+  description = "This enables creation of a stream event source mapping for the Lambda function. Defaults to false."
+  default     = false
+}
+
+variable "stream_event_source_arn" {
+  description = "An optional event source ARN - can either be a Kinesis or DynamoDB stream."
   default     = ""
+}
+
+variable "stream_starting_position" {
+  description = "The position in the stream where AWS Lambda should start reading. Must be one of either TRIM_HORIZON or LATEST. Defaults to TRIM_HORIZON."
+  default     = "TRIM_HORIZON"
 }
 
 variable "tags" {
   description = "A mapping of tags to assign to the Lambda function."
   type        = "map"
   default     = {}
+}
+
+variable "timeout" {
+  description = "The amount of time your Lambda Function has to run in seconds. Defaults to 3."
+  default     = 3
 }
