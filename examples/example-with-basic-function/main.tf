5981c32915c9b061b6d0841ef10bd30b0dfffb97
Moritz Zimmer <moritz.zmmr@gmail.com>
2018-11-13 17:09:22 +0100

    Use lambda submodule in basic example

--- examples/example-with-basic-function/main.tf
+++ examples/example-with-basic-function/main.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 module "lambda" {
-  source        = "../../"
+  source        = "../../modules/lambda"
   handler       = "some-handler"
   function_name = "handler"
   description   = "Example for AWS Lambda w/o event trigger."
   s3_bucket     = "some-bucket"
   s3_key        = "v1.0.0/handler.zip"
   memory_size   = "512"
-  runtime       = "java8"
+  runtime       = "go1.x"
   timeout       = 120
 
   tags {
