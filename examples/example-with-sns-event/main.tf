b35b13e35767f4b38637f7343fa07b6aa2686edf
Moritz Zimmer <moritz.zmmr@gmail.com>
2019-07-18 13:26:02 +0200

    Added support for SNS as event trigger

---
+++ examples/example-with-sns-event/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "lambda" {
+  source        = "../../"
+  description   = "Example AWS Lambda using go with sns trigger"
+  filename      = "test_function.zip"
+  function_name = "tf-example-go-sns"
+  handler       = "example-lambda-func"
+  runtime       = "go1.x"
+
+  event {
+    type      = "sns"
+    topic_arn = "arn:aws:sns:eu-west-1:123456789:test-topic"
+  }
+
+  tags {
+    key = "value"
+  }
+
+  environment {
+    variables {
+      key = "value"
+    }
+  }
+}
---
+++ modules/event/sns/variables.tf
@@ -0,0 +1,16 @@
+variable "enable" {
+  description = "Conditionally enables this module (and all it's ressources)."
+  default     = 0
+}
+
+variable "endpoint" {
+  description = "The endpoint to send data to (ARN of the Lambda function)"
+}
+
+variable "function_name" {
+  description = "Name of the Lambda function whose resource policy should be allowed to subscribe to SNS topics."
+}
+
+variable "topic_arn" {
+  description = "The ARN of the SNS topic to subscribe to"
+}
