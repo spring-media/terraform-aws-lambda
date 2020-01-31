e189d2711fbdfb277fc3df2aec0113bf84652f61
Moritz Zimmer <moritz.zmmr@gmail.com>
2020-01-31 17:40:10 +0100

    lock down of kinesis permission
    see official sam iam templates for the why:
    https://github.com/awslabs/serverless-application-model/blob/develop/samtranslator/policy_templates_data/policy_templates.json

--- modules/event/kinesis/main.tf
+++ modules/event/kinesis/main.tf
@@ -1,3 +1,9 @@
+data "aws_region" "current" {
+}
+
+data "aws_caller_identity" "current" {
+}
+
 resource "aws_lambda_event_source_mapping" "stream_source" {
   count             = var.enable ? 1 : 0
   batch_size        = var.batch_size
@@ -7,8 +13,41 @@ resource "aws_lambda_event_source_mapping" "stream_source" {
   starting_position = var.starting_position
 }
 
-resource "aws_iam_role_policy_attachment" "kinesis" {
+data "aws_iam_policy_document" "stream_policy_document" {
+  statement {
+    actions = [
+      "kinesis:ListStreams",
+      "kinesis:DescribeLimits"
+    ]
+
+    resources = [
+      "arn:aws:kinesis:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:stream/*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "kinesis:DescribeStream",
+      "kinesis:DescribeStreamSummary",
+      "kinesis:GetRecords",
+      "kinesis:GetShardIterator"
+    ]
+
+    resources = [
+      "arn:aws:kinesis:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:stream/${var.stream_name}",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "stream_policy" {
+  count       = var.enable ? 1 : 0
+  name        = "${var.function_name}-stream-consumer"
+  description = "Gives permission to list and read a Kinesis stream to ${var.function_name}."
+  policy      = data.aws_iam_policy_document.stream_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "stream_policy_attachment" {
   count      = var.enable ? 1 : 0
   role       = var.iam_role_name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaKinesisExecutionRole"
+  policy_arn = aws_iam_policy.stream_policy[count.index].arn
 }
