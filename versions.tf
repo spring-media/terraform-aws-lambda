e5bcbd944766a9b9d8d2b38129d6429956d35c59
Halima Sani <hsani@redventures.com>
2022-09-26 12:50:20 -0500

    updated terraform and aws version pins

--- versions.tf
+++ versions.tf
@@ -1,4 +1,9 @@
-
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=4.0"
+    }
+  }
 }
